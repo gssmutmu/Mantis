6e5197f9e9894cf8dbeb29de6f28876884e67fa1
Muthuselvan Murugan <Muthuselvan.Murugan@flex.com>
2018-05-07 00:02:04 -0700

    [Webexplorer] Create node "isLogin", Update node "isLogin", Connect screens to page model "login"

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const session = require('smartux-connect');
 session.transform.mantis = session.loadTransform('mantis');
 require('./client');
+require('./transform/mantis/login');
 session.start();
