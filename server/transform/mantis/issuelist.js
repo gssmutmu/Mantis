68f104f8a225623a8051286af3e61f827379e10f
Muthuselvan Murugan <Muthuselvan.Murugan@flex.com>
2018-05-07 02:25:50 -0700

    [Webexplorer] Delete connection from page model "issuelist", Connect screens to page model "issuelist"

--- server/transform/mantis/issuelist.js
+++ server/transform/mantis/issuelist.js
@@ -2,3 +2,7 @@ exports.issuelist = async (session, models, vars) => {
     models.issuelist = vars.page;
     await session.screen('issuelist');
 };
+exports.issuelist2 = async (session, models, vars) => {
+    models.issuelist = vars.page;
+    await session.screen('issuelist');
+};
